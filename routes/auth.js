dea042d7872bba4f894bbfdabbfb2fd6815301f4
Jose Luis Hidalgo <joseluishid01@gmail.com>
2022-11-17 17:46:15 -0400

    Seccion 12 - Google

--- routes/auth.js
+++ routes/auth.js
@@ -3,17 +3,26 @@
 */
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 
 router.post('/',[
-    check('email','El email es Obligatorio').isEmail(),
-    check('password','La password es Obligatoria').not().isEmpty(),
-    validarCampos
-],login)
+        check('email','El email es Obligatorio').isEmail(),
+        check('password','La password es Obligatoria').not().isEmpty(),
+        validarCampos
+    ],
+    login
+)
+
+router.post('/google',[
+        check('token','La token es Obligatoria').not().isEmpty(),
+        validarCampos
+    ],
+    googleSignIn
+)
 
 
 
